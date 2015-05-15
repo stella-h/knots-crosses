7a151ba8352ad23513179fe9e4418083e676b2bb
stella <stella@stellas-MacBook-Pro.local>
2015-05-15 10:32:18 +1000

    update styling

--- js/main.js
+++ js/main.js
@@ -1,33 +1,39 @@
 var move = 0;
 var id = 0;
 
-//        player turn function        //
+
+/*
+     _   _   _   _   _   _     _   _   _   _     _   _   _   _   _   _   _   _   
+    / \ / \ / \ / \ / \ / \   / \ / \ / \ / \   / \ / \ / \ / \ / \ / \ / \ / \  
+   ( P | L | A | Y | E | R ) ( M | O | V | E ) ( F | U | N | C | T | I | O | N ) 
+    \_/ \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/  
+*/
+
 var playerMove = function() { 
+
     // If it doesnt have the class selected, do everything in this function
     // If it didnt have the class selected (if it got into the if statement)
     // Add the class selected to that element
     // The reason we do this is so that the next time this function runs, this element will have the class selected and not go into the if statement
 
 
-
-
-if (!$(this).hasClass('selected')) {  //logical operator !, if it doesn't have the class selected. then
-$(this).addClass('selected')        // if this doesn't the class selected THEN add the class
+if (!$(this).hasClass('selected')) {   
+$(this).addClass('selected')           // if this doesn't the class selected THEN add the class
   if (move === 0) {
     $(this).html('X');
     move += 1;
     id = parseInt($(this).attr('id')) -1 ; // pushing the id of the box into the array
-    gameValue[id] = 'X'; // inserting the value to the key
-    console.log(id); //the key to assign the value 
-    console.log(gameValue); //display the array 
+    gameValue[id] = 'X';                   // inserting the value to the key
+    console.log(id);                       //the key to assign the value 
+    console.log(gameValue);                //display the array 
 
   } else {
+    move -= 1;  
     $(this).html('O');
-    move -= 1;
-    id = parseInt($(this).attr('id')) -1 ; //change the string into a number
-    gameValue[id] = 'O'; // push the ID into the array
-    console.log(id); //
-    console.log(gameValue); //display the array
+    id = parseInt($(this).attr('id')) -1 ;  //change the string into a number
+    gameValue[id] = 'O';                    // push the ID into the array
+    console.log(id); 
+    console.log(gameValue);                 //display the array
   }
   endGame(); }
 };
@@ -36,26 +42,28 @@ $(this).addClass('selected')        // if this doesn't the class selected THEN a
 $(".gameBoard").on('click', playerMove);
 
 
-// creating an array to store the moves, subsequently to determined who wins the game. 
+// creating an array to store the moves.
 
 var gameValue = ["", "", "", "", "", "", "", "", ""]
 
 
 
-//        win game function        //
+/*
+     _   _   _     _   _   _   _     _   _   _   _   _   _   _   _   
+    / \ / \ / \   / \ / \ / \ / \   / \ / \ / \ / \ / \ / \ / \ / \  
+   ( W | I | N ) ( G | A | M | E ) ( F | U | N | C | T | I | O | N ) 
+    \_/ \_/ \_/   \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/  
+*/
 
-var winGame = function(a, b, c) { // spit out the results so it can be used again.
+var winGame = function(a, b, c) {             // spit out the results so it can be used again.
   return $('#' + a).text() + $('#' + b).text() + $('#' + c).text();
 }
-
-
 
 var noMove = function (a,b,c) {
   if ($('#' + a).text() === ('X') || $('#' + b).text  || $('#' + c).text === O || X ); {
     console.log($('#sidePanel').html('that move is not allowed'));
   }
 };
-
 
 var endGame = function() {
   if (winGame(1, 2, 3) === 'XXX') {
@@ -110,7 +118,13 @@ var endGame = function() {
 };
 
 
-//          reset game function      ///
+/*
+#     _   _   _   _   _     _   _   _   _     _   _   _   _   _   _   _   _   
+#    / \ / \ / \ / \ / \   / \ / \ / \ / \   / \ / \ / \ / \ / \ / \ / \ / \  
+#   ( R | E | S | E | T ) ( G | A | M | E ) ( F | U | N | C | T | I | O | N ) 
+#    \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ 
+*/
+
 var resetGame = function () {
   $('.gameBoard').html('');
   gameValue = ["", "", "", "", "", "", "", "", ""]; //resets array 
@@ -123,7 +137,42 @@ var resetGame = function () {
 $("#reset").on('click', resetGame);
 
 
+/*
+   _    _    _     _    _    _    _    _     _    _    _    _    _    _    _  
+  / \  / \  / \   / \  / \  / \  / \  / \   / \  / \  / \  / \  / \  / \  / \ 
+ ( B )( O )( X ) ( H )( O )( V )( E )( R ) ( O )( P )( A )( C )( I )( T )( Y )
+  \_/  \_/  \_/   \_/  \_/  \_/  \_/  \_/   \_/  \_/  \_/  \_/  \_/  \_/  \_/ 
+*/
+
+var overBox = function() {
+  $(this).css({
+    'opacity': 0.4
+  });
+};
+
+var offBox = function() {
+  $(this).css({
+    'opacity': 1
+  });
+};
+
+$('.gameBoard').on('mouseover', overBox);
+$('.gameBoard').on('mouseleave', offBox);
 
 
 
 
+
+// var gameCursor = function() {
+
+//   if (move === 0) {
+//     $('gameBoard').css({'cursor':'url(http://i.imgur.com/weu8kXG.png), auto'});
+//     move += 1;
+//   } else {
+//     $('gameBoard').css({'cursor':'url(http://i.imgur.com/weu8kXG.png), auto'});
+//     move -=1;
+//   }
+// };
+
+//<a href="http://imgur.com/wVFMQwM"><img src="http://i.imgur.com/wVFMQwM.gif" title="source: imgur.com" /></a
+//$(this).append('<img src="http://i.imgur.com/wVFMQwM.gif">');
---
+++ readme.md
@@ -0,0 +1,24 @@
+  .                   .                        .                 
+ _/_   `   ___       _/_     ___    ___       _/_     __.    ___ 
+  |    | .'   `       |     /   ` .'   `       |    .'   \ .'   `
+  |    | |            |    |    | |            |    |    | |----'
+  \__/ /  `._.'       \__/ `.__/|  `._.'       \__/  `._.' `.___,
+                                                                 
+
+features
+- announces the winner
+- resets game button
+- opacity effect on selected square
+- disable clicking on filled square
+
+approach 
+- 
+
+if given more time, some features that would be good to add,
+- player move indicator (who's turn)
+- mouse indicator (which piece to be drawn next)
+- recognises draw game and offer a restart game option
+- alert/visual cues if the box has been filled. 
+- animated player piece
+- score board
+- play with cpu option. 
